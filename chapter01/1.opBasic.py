236c97c1de47c6823f3064dd2c5d8fd6dbc8494d
objectiveid <objectiveid@naver.com>
2019-11-25 14:33:30 +0900

    teacher's files uploaded

---
+++ chapter01/1.opBasic.py
@@ -0,0 +1,45 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Mon Nov 25 11:35:07 2019
+
+@author: edu
+"""
+
+# 정수형 Max 값?? 
+a = 123456789012345678901234567890
+print(a)
+
+# 실수형 Max 값?? 
+b = 1.12345678901234567890
+print(b)
+
+print('abc', "def")
+print('abc'+"def")
+
+print('abc', b)
+#print('abc'+ b)  # error
+
+myInt = 123
+myStr = '1456'
+print(str(myInt) + myStr)
+
+name = input('Name? ')
+print('name --> ', name)
+
+print("Name? ")
+name = input();
+print('name --> ', name)
+
+
+
+
+
+
+
+
+
+
+
+
+
+
