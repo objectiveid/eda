bca5cf45b56fee5cf17cd57f32a09a23a535fb0f
objectiveid <objectiveid@naver.com>
2019-11-25 13:31:33 +0900

    day1branch1code

---
+++ chapter01/1_opBasic.py
@@ -0,0 +1,17 @@
+# -*- coding: utf-8 -*-
+"""
+Spyder Editor
+
+This is a temporary script file.
+"""
+
+a = 1234123142251251555678901241354663142124214124124
+print (a)
+b = 10.271231535361613161512241421421411421424121251531
+print(b)
+
+print(abc)
+
+myint = 1234
+mystr = '123'
+print (str(myint) + mystr)
